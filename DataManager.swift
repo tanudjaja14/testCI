3146a34fe9345fbae4a7fb5a8f39604f4cb5d692
wimRigil <tanudjaja@mac.com>
2020-01-23 15:09:50 -0500

    First Commit

---
+++ DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  CollaborationTest
+//
+//  Created by Wim Tanudjaja on 1/23/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
