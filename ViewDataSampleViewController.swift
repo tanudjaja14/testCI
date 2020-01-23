59e4f5d2f5b163bf8db676e22347f21f7371b405
tanudjaja14 <wim@rigil.com>
2020-01-23 15:15:47 -0500

    other contributor

---
+++ ViewDataSampleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewDataSampleViewController.swift
+//  CollaborationTest
+//
+//  Created by Sean Rada on 1/23/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewDataSampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
